37b80dd30cc44ecf9ec7190b2098da7f3f435b83
tinnv2 <tinnv2@gmail.com>
2021-12-03 22:01:00 +0700

    add message.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Parrot import Parrot
 from model.Message import Message
+from service.MessageService import MessageService
 
 class main:
     tin = Parrot("Tin", 23)
@@ -9,5 +10,7 @@ class main:
     nam = Parrot("Nam", 24)
     print("{} is {} years old.".format(nam.name, nam.age))
 
-    m = Message("1", "2", "3", "4").ConverTostring()
+    m = Message("1", "2", "3", "4", "1", "2").ConverTostring()
+
+    msgService = MessageService()
     print(m)
